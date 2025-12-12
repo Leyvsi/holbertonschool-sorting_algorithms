0f5ac118644e79ea3f2e9da746c6506b9531f7a6
leyvsi <sylviecosta78500@gmail.com>
2025-12-12 11:25:17 +0000

    add 0-bubble_sort.c and up sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,11 +9,11 @@
  */
 void swap_elements(int *a, int *b)
 {
-    int tmp;
+	int tmp;
 
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -25,29 +25,29 @@ void swap_elements(int *a, int *b)
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int swapped;
+	size_t i, j;
+	int swapped;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
 
-        for (j = 0; j < size - 1 - i; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                swap_elements(&array[j], &array[j + 1]);
-                swapped = 1;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap_elements(&array[j], &array[j + 1]);
+				swapped = 1;
 
-                /* Required action: Print array after each swap */
-                print_array((const int *)array, size);
-            }
-        }
+				/* Required action: Print array after each swap */
+				print_array((const int *)array, size);
+			}
+		}
 
-        if (swapped == 0)
-            break;
-    }
+		if (swapped == 0)
+			break;
+	}
 }
--- sort.h
+++ sort.h
@@ -4,18 +4,24 @@
 #include <stddef.h>
 #include <stdio.h>
 
-/* Print functions */
-void print_array(const int *array, size_t size);
-void print_list(const struct listint_s *list);
-
 /* Doubly linked list structure */
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
+/* Print functions */
+void print_list(const struct listint_s *list);
+void print_array(const int *array, size_t size);
 /* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
